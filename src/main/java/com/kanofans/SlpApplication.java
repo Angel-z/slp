2811e44e23002438ec1d132797604148ffbe35bb
Angel <52435162+Angel-z@users.noreply.github.com>
2024-04-22 00:14:27 +0800

    自签https

--- src/main/java/com/kanofans/SlpApplication.java
+++ src/main/java/com/kanofans/SlpApplication.java
@@ -1,8 +1,17 @@
 package com.kanofans;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.apache.tomcat.websocket.server.WsSci;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 /**
  * 启动程序
@@ -17,5 +26,43 @@ public class SlpApplication
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(SlpApplication.class, args);
         System.out.println("slp启动成功\n");
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+    }
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8999);
+        connector.setSecure(false);
+        connector.setRedirectPort(9000);
+        return connector;
+    }
+
+    @Bean
+    public TomcatContextCustomizer tomcatContextCustomizer() {
+        System.out.println("TOMCATCONTEXTCUSTOMIZER INITILIZED");
+        return new TomcatContextCustomizer() {
+            @Override
+            public void customize(Context context) {
+                context.addServletContainerInitializer(new WsSci(), null);
+            }
+
+        };
     }
 }
